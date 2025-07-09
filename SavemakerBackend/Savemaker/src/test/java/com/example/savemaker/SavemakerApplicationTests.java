50ef877d4ee93a808aa47acfca4a947b5c1dddf5
Matijas Levang <micolevang@gmail.com>
2025-07-09 15:08:17 +0200

    Initial commit

---
+++ SavemakerBackend/Savemaker/src/test/java/com/example/savemaker/SavemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
