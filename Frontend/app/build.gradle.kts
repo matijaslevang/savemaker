b7177a39d552129a949308f1d8e36ad48c85c773
Matijas Levang <micolevang@gmail.com>
2025-07-12 18:46:58 +0200

    [ADD] Retrofit HTTP client on frontend

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -1,5 +1,18 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
+}
+
+fun getIpAddress(): String? {
+    val properties = Properties()
+    val localPropertiesFile = File("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { inputStream ->
+            properties.load(inputStream)
+        }
+    }
+    return properties.getProperty("ip_addr")
 }
 
 android {
@@ -7,28 +20,25 @@ android {
     compileSdk = 35
 
     defaultConfig {
+
         applicationId = "com.example.savemaker"
         minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        buildConfigField("String", "IP_ADDR", "\""+getIpAddress()+"\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     viewBinding {
         enable = true
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -46,4 +56,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.3.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
 }
