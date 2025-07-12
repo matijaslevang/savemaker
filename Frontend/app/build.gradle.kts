ea462b2d4bdb07feaa082f0530f673d58f4885d7
Matijas Levang <micolevang@gmail.com>
2025-07-12 17:34:59 +0200

    [ADD] Drawer menu

--- Frontend/app/build.gradle.kts
+++ Frontend/app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.ui)
+    implementation(libs.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
