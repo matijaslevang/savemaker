87eecaf81cd40c628bf4eff73d9a0801e5433ada
Matijas Levang <micolevang@gmail.com>
2025-07-21 15:51:24 +0200

    [ADD] Priorty list editing to settings

--- Frontend/app/src/main/java/com/example/savemaker/settings/SettingsFragment.java
+++ Frontend/app/src/main/java/com/example/savemaker/settings/SettingsFragment.java
@@ -9,58 +9,55 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.savemaker.R;
+import com.example.savemaker.databinding.FragmentSettingsBinding;
+import com.example.savemaker.settings.dialogs.PriorityListDialog;
+import com.example.savemaker.settings.models.PriorityListElement;
+import com.example.savemaker.utils.ClientUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentSettingsBinding binding;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+
+        binding.settingsEditPriorityListButton.setOnClickListener(v -> {
+            Call<List<PriorityListElement>> call = ClientUtils.settingsService.getPriorityList();
+            call.enqueue(
+                    new Callback<List<PriorityListElement>>() {
+                        @Override
+                        public void onResponse(Call<List<PriorityListElement>> call, Response<List<PriorityListElement>> response) {
+                            if (response.isSuccessful()) {
+                                PriorityListDialog dialog = new PriorityListDialog(requireContext(), response.body());
+                                dialog.show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<PriorityListElement>> call, Throwable t) {
+
+                        }
+                    }
+            );
+        });
+
+        return binding.getRoot();
     }
 }
