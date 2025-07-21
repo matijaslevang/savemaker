03fd137d8397a9fb62d9291f948b6f76d8733730
Matijas Levang <micolevang@gmail.com>
2025-07-21 20:49:52 +0200

    [ADD] Find category by whether it is used for income

--- Backend/Savemaker/src/main/java/com/example/savemaker/transactions/repositories/CategoryRepository.java
+++ Backend/Savemaker/src/main/java/com/example/savemaker/transactions/repositories/CategoryRepository.java
@@ -3,6 +3,13 @@ package com.example.savemaker.transactions.repositories;
 
 import com.example.savemaker.transactions.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("SELECT c from Category c WHERE c.isUsedForIncome = :usedForIncome")
+    List<Category> findAllByUsedForIncome(@Param("usedForIncome") Boolean usedForIncome);
 }
--- Backend/Savemaker/src/main/java/com/example/savemaker/transactions/services/CategoryService.java
+++ Backend/Savemaker/src/main/java/com/example/savemaker/transactions/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.example.savemaker.transactions.services;
 
+import com.example.savemaker.settings.dtos.PreferredPriorityElementDTO;
 import com.example.savemaker.transactions.models.Category;
 import com.example.savemaker.transactions.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,22 @@ public class CategoryService {
 
     public List<Category> getAll() {
         return categoryRepository.findAll();
+    }
+
+    public List<Category> getAllByUsedForIncome(Boolean usedForIncome) {
+        return categoryRepository.findAllByUsedForIncome(usedForIncome);
+    }
+
+    public Boolean updatePreferredPriorities(List<PreferredPriorityElementDTO> dtos) {
+        for (PreferredPriorityElementDTO dto : dtos) {
+            Category current = getCategory(dto.getSpendingCategoryId());
+            if (current == null || dto.getPreferredIncomeCategory() == null) return false;
+            if (!dto.getPreferredIncomeCategory().getUsedForIncome()) return false;
+
+            current.setPreferredSpendingCategory(getCategory(dto.getPreferredIncomeCategory().getId()));
+            categoryRepository.save(current);
+        }
+        return true;
     }
 
 }
